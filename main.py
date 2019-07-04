2c2e320a506adefcdaf3cd3b62bda8d0c6696b0e
unknown <juliane.hansmann@gmx.de>
2019-07-04 17:29:08 +0200

    added background story

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from general import *
 from adventurelib import *
 
+say("Good evening, my dearest Thrablin.")
+say("Just a few years ago, our dear Friend Balin departed with a few of ours to take back the wonderful kingdom of Khazad-dûm. Among the ones who joined him, there also was Nadihm, your sister's son. As we all found out last year, there attemps to take back what was rightfully ours, failed and our friends were doomed to die in the mines. But now I received a glimpse of hope: There are rumours about confused beeings that have been spotted between Lórien and the former entry to the mines.")
+say("I have chosen you as the most fitting one for the quest to find out if there are any survivors in hope that you will find some or at least find closure in the grieving for your beloved nephew.")
+say("While searching the area, please keep in mind that treasures of the dwarfs beyond measure also got lost while fighting the orcs. I expect that you will be able to retrieve at least some of these.")
+say("I expect you to leave for this quest within the next two weeks. If you are in need of any assistance, I will provide you with whatever I can.")
+say("Dáin")
+say("King under the Mountain")
+say("")
+say("Following the whishes of your king, you started your journey...")
+say("")
 look()
 start()
