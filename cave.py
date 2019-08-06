ed25c0f314f07ed20b1858d9fa41c310f661e3f0
unknown <juliane.hansmann@gmx.de>
2019-08-06 21:23:29 +0200

    added the missing items and sorted them into the rooms

--- cave.py
+++ cave.py
@@ -11,9 +11,15 @@ key = Item('key', 'key')
 letter = Item('letter', 'a letter from Dàin')
 moonstone = Item('moonstone', 'moonstone')
 runepaper = Item('runepaper', 'runepaper')
-winebottle = Item('bottle', 'bottle')
-
-
+winebottle = Item('winebottle', 'bottle')
+longbow = Item('longbow','bow')
+arrows = Item('arrows', 'arrows')
+ham = Item('ham', 'ham')
+spoon = Item('spoon', 'spoon')
+ring = Item('ring with elven writing', 'ring')
+beads = Item('some golden beads', 'beads')
+gems = Item('some beautiful gemstones', 'gemstones', 'gems')
+sword = Item('very rusty sword', 'sword')
 
 
 """define characters"""
@@ -23,30 +29,35 @@ Nadihm = Character('Nadihm', 'Nadihm')
 Fundor = Character('Fundor', 'Fundor')
 Frain = Character('Frain', 'Frain')
 
-"""define the rooms available, their descriptions, contained items and connections"""
+
+"""define the rooms available, their descriptions, contained items, people and connections"""
 
 hall = Room("""You are in a hall""")
 hall.characters = Group({Fundor,})
+hall.items = Bag({longbow,})
 
-living = hall.north = Room("""You are in a living room""")
+living = hall.north = Room("""You are in room which seems to be used as living room. But you don't care too much for the interior as you your nephew fletching some arrows... """)
 living.characters = Group({Nadihm,})
+living.items = Bag({arrows,})
 
-supplyI = hall.east = Room("""You are in supply room containing tools and stuff""")
+supplyI = hall.east = Room("""You are in supply room containing tools and other useful stuff.""")
+supplyI.items = Bag({axe,})
 
 dining = living.east = Room("""You are in the dining room""")
-dining.items = Bag({moonstone,})
+dining.items = Bag({ring, moonstone, beads, gems})
 
 kitchen = dining.north = Room("""You are in the kitchen""")
 kitchen.characters = Group({Frain,})
+kitchen.items = Bag({spoon,})
 
 supplyII = kitchen.east = Room("""You are in a supply room with food and drinks.""")
-supplyII.items = Bag({winebottle,})
+supplyII.items = Bag({winebottle,ham})
 
 sleeping = kitchen.west = Room("""You are small room containing three sleeping arrangements""")
-sleeping.items = Bag({axe,runepaper,})
+sleeping.items = Bag({runepaper,})
 
 treasure = Room("""You have finally found the treaure room. Yay!""")
-
+treasure.items = Bag({sword,})
 
 
 
